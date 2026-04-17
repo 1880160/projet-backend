00000602387e4e1e029fd1804bc2db826037fc60
Cedric <you@example.com>
2026-04-17 13:01:03 -0400

    add workout documentation

--- src/workout/dto/create-workout.dto.ts
+++ src/workout/dto/create-workout.dto.ts
@@ -1,17 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { Type } from "class-transformer"
 import { IsOptional, IsString, IsNotEmpty, IsDate, IsNumber, IsEmpty, IsDateString, IsArray } from "class-validator"
 import { UserExercise } from "src/user-exercises/entities/user-exercise.entity"
 import { User } from "src/users/user/user.entity"
 
 export class CreateWorkoutDto {
+        @ApiProperty(
+                {example : "Leg day"}
+        )
         @IsString()
         @IsNotEmpty()
         workoutName : string
-    
+        @ApiProperty({
+        description : "Day / of the workout"
+
+        })
         @Type(() => Date)
         @IsDate()
         weekDate : Date
-    
+        @ApiProperty({
+        description : "Time to alert for the next workout"
+        })
         @IsDate()
         @Type(() => Date)
         @IsOptional()
@@ -21,7 +30,9 @@ export class CreateWorkoutDto {
         @IsEmpty()
         user : User
 
-
+        @ApiProperty({
+                description : "user Exercises to add to the workout"
+        })
         @IsNumber({},{each : true})
         @IsOptional()
         userExercisesId : number[]
